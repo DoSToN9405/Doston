6b43400de24ab3d89a38179d33de9d8e117942e4
DaSToN <73336402+DASTONBE@users.noreply.github.com>
2024-05-05 18:09:53 +0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,93 @@
+import { useWalletModal } from '@solana/wallet-adapter-react-ui'
+import { useTransactionError } from 'gamba-react-v2'
+import React from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import { Modal } from './components/Modal'
+import { StyledSection } from './components/Slider'
+import { useToast } from './hooks/useToast'
+import Dashboard from './sections/Dashboard/Dashboard'
+import Game from './sections/Game/Game'
+import Header from './sections/Header'
+import RecentPlays from './sections/RecentPlays/RecentPlays'
+import Toasts from './sections/Toasts'
+import { useUserStore } from './hooks/useUserStore'
+import { GambaUi } from 'gamba-react-ui-v2'
+
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  React.useEffect(() => window.scrollTo(0, 0), [pathname])
+  return null
+}
+
+function ErrorHandler() {
+  const walletModal = useWalletModal()
+  const toast = useToast()
+  const [error, setError] = React.useState<Error>()
+
+  useTransactionError(
+    (error) => {
+      if (error.message === 'NOT_CONNECTED') {
+        walletModal.setVisible(true)
+        return
+      }
+      toast({ title: '❌ Transaction error', description: error?.error?.errorMessage ?? error.message })
+    },
+  )
+
+  return (
+    <>
+      {error && (
+        <Modal onClose={() => setError(undefined)}>
+          <h1>Error occured</h1>
+          <p>{error.message}</p>
+        </Modal>
+      )}
+    </>
+  )
+}
+
+export default function App() {
+  const newcomer = useUserStore((state) => state.newcomer)
+  const set = useUserStore((state) => state.set)
+  return (
+    <>
+      {newcomer && (
+        <Modal>
+          <h1>Welcome</h1>
+          <div style={{ position: 'relative' }}>
+            <div style={{ maxHeight: '400px', padding: '10px', overflow: 'auto', position: 'relative' }}>
+              <p><b>1. Age Requirement:</b> Must be at least 18 years old.</p>
+              <p><b>2. Legal Compliance:</b> Follow local laws responsibly.</p>
+              <p><b>3. Risk Acknowledgement:</b> Games involve risk; no guaranteed winnings.</p>
+              <p><b>4. No Warranty:</b> Games provided "as is"; operate randomly.</p>
+              <p><b>5. Limitation of Liability:</b> We're not liable for damages.</p>
+              <p><b>6. Licensing Disclaimer:</b> Not a licensed casino; for simulation only.</p>
+              <p><b>7. Fair Play:</b> Games are conducted fairly and transparently.</p>
+              <p><b>8. Data Privacy:</b> Your privacy is important to us.</p>
+              <p><b>9. Responsible Gaming:</b> Play responsibly; seek help if needed.</p>
+            </div>
+            <div style={{ background: 'linear-gradient(180deg, transparent, #15151f)', height: '50px', pointerEvents: 'none', width: '100%', position: 'absolute', bottom: '0px', left: '0px' }}></div>
+          </div>
+          <p>
+            By playing on our platform, you confirm your compliance.
+          </p>
+          <GambaUi.Button main onClick={() => set({ newcomer: false })}>
+            Acknowledge
+          </GambaUi.Button>
+        </Modal>
+      )}
+      <ScrollToTop />
+      <ErrorHandler />
+      <Header />
+      <Toasts />
+      <StyledSection>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/:gameId" element={<Game />} />
+        </Routes>
+        <h2 style={{ textAlign: 'center' }}>Recent Plays</h2>
+        <RecentPlays />
+      </StyledSection>
+    </>
+  )
+}
---
+++ src/constants.ts
@@ -0,0 +1,87 @@
+import { PublicKey } from '@solana/web3.js'
+import { FAKE_TOKEN_MINT, PoolToken, TokenMeta, makeHeliusTokenFetcher } from 'gamba-react-ui-v2'
+
+// Get RPC from the .env file or default to the public RPC.
+export const RPC_ENDPOINT = import.meta.env.VITE_RPC_ENDPOINT ?? 'https://api.mainnet-beta.solana.com'
+
+// Solana address that will receive fees when somebody plays on this platform
+export const PLATFORM_CREATOR_ADDRESS = new PublicKey(
+  'V2grJiwjs25iJYqumbHyKo5MTK7SFqZSdmoRaj8QWb9'
+)
+
+// Gamba explorer URL - Appears in RecentPlays
+export const EXPLORER_URL = `https://explorer.gamba.so/`;
+
+// Platform URL - Appears in ShareModal
+export const PLATFORM_SHARABLE_URL = 'play.gamba.so'
+
+// Creator fee (in %)
+export const PLATFORM_CREATOR_FEE = 0.05 // 1% (1/100 = 0.01)
+
+// Jackpot fee (in %)
+export const PLATFORM_JACKPOT_FEE = 0.001 // 0.1% (0.1/100 = 0.001)
+
+// Just a helper function
+const lp = (tokenMint: PublicKey | string, poolAuthority?: PublicKey | string): PoolToken => ({
+  token: new PublicKey(tokenMint),
+  authority: poolAuthority !== undefined ? new PublicKey(poolAuthority) : undefined,
+})
+
+/**
+ * List of pools supported by this platform
+ * Make sure the token you want to list has a corresponding pool on https://explorer.gamba.so/pools
+ * For private pools, add the creator of the Liquidity Pool as a second argument
+ */
+export const POOLS = [
+  // SOL:
+  lp('So11111111111111111111111111111111111111112'),
+  // USDC:
+  lp('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'),
+  // Fake token:
+  lp(FAKE_TOKEN_MINT),
+  // Wormhole:
+  lp('85VBFQZC9TZkfaptBWjvUw7YbZjy52A6mjtPGjstQAmQ'),
+]
+
+// The default token to be selected
+export const DEFAULT_POOL = POOLS[0]
+
+/**
+ * List of token metadata for the supported tokens
+ * Alternatively, we can provide a fetcher method to automatically fetch metdata. See TOKEN_METADATA_FETCHER below.
+ */
+export const TOKEN_METADATA: (Partial<TokenMeta> & {mint: PublicKey})[] = [
+  {
+    mint: FAKE_TOKEN_MINT,
+    name: 'Fake',
+    symbol: 'FAKE',
+    image: '/fakemoney.png',
+    baseWager: 1e9,
+    decimals: 9,
+    usdPrice: 0,
+  },
+  {
+    mint: new PublicKey('85VBFQZC9TZkfaptBWjvUw7YbZjy52A6mjtPGjstQAmQ'),
+    name: 'W',
+    symbol: 'Wormhole',
+    image: 'https://wormhole.com/token.png',
+    baseWager: 1e6,
+    decimals: 6,
+    usdPrice: 0,
+  },
+]
+
+/**
+ * A method for automatically fetching Token Metadata.
+ * Here we create a fetcher that uses Helius metadata API, if an API key exists as an environment variable.
+ */
+export const TOKEN_METADATA_FETCHER = (
+  () => {
+    if (import.meta.env.VITE_HELIUS_API_KEY) {
+      return makeHeliusTokenFetcher(
+        import.meta.env.VITE_HELIUS_API_KEY,
+        { dollarBaseWager: 1 },
+      )
+    }
+  }
+)()
